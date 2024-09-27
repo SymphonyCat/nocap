d651415a3c3b117e4caf3617f9bb1a82bc249dcb
Rodrigo <101577451+SymphonyCat@users.noreply.github.com>
2024-09-27 14:48:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,51 @@ os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 INITIAL_RESPONSE = secrets["INITIAL_RESPONSE"]
 INITIAL_MSG = secrets["INITIAL_MSG"]
 CHAT_CONTEXT = secrets["CHAT_CONTEXT"]
+# Initialize the chat history if present as Streamlit session
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        {"role": "assistant",
+         "content": INITIAL_RESPONSE
+         },
+    ]
+
+client = Groq()
+
+# Page title
+st.title("Que tal")
+st.caption("CircuitSage")
+
+# Display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message("role", avatar=''):
+        st.markdown(message["content"])
+
+user_prompt = st.chat_input("Let's chat!")
+
+def parse_groq_stream(stream):
+    for chunk in stream:
+        if chunk.choices:
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
+
+if user_prompt:
+    with st.chat_message("user", avatar=""):
+        st.markdown(user_prompt)
+    st.session_state.chat_history.append(
+        {"role": "user", "content": user_prompt})
+
+    messages = [
+        {"role": "system", "content": CHAT_CONTEXT
+         },
+        {"role": "assistant", "content": INITIAL_MSG},
+        *st.session_state.chat_history
+    ]
+
+    stream = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=messages,
+        stream=True  # for streaming the message
+    )
+    response = st.write_stream(parse_groq_stream(stream))
+    st.session_state.chat_history.append(
+        {"role": "assistant", "content": response})
